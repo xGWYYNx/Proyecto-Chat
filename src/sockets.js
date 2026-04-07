dcf4a9da79f1bff5aa7b786691b78379a876a168
xGWYYNx <dilanmateop@gmail.com>
2026-04-07 06:25:08 -0500

    cambios finales

--- src/sockets.js
+++ src/sockets.js
@@ -1,21 +1,22 @@
-
 const Chat = require('./Models/Chat');
 
 module.exports = function(io){
 
-    let users ={};
+    let users = {};
 
-    io.on('connection', async socket =>{
-        console.log("Nuevo Usuario conectado ");
+    io.on('connection', async socket => {
+        console.log("Nuevo Usuario conectado");
 
         let messages = await Chat.find({ msg: { $not: /^\/w / } })
-                        .limit(8)
-                        .sort({ created_at: 1 })
-                        .lean();  
+            .sort({ created_at: -1 })
+            .limit(8)
+            .lean();
+
+        messages.reverse();
 
         socket.emit('Cargando viejos mensajes', messages);
 
-        socket.on('Nuevo usuario', (data, cb)=>{
+        socket.on('Nuevo usuario', (data, cb) => {
             if(data in users){
                 cb(false);
             }else{
@@ -26,48 +27,66 @@ module.exports = function(io){
             }
         });
 
-
-        socket.on("Enviar mensaje", async (data,cb) => {
+        socket.on("Enviar mensaje", async (data, cb) => {
             var msg = data.trim();
-            if (msg.startsWith('/w ')){
+
+            if(msg.startsWith('/w ')){
                 msg = msg.substr(3);
                 const index = msg.indexOf(' ');
-                if (index != -1){
+
+                if(index != -1){
                     var name = msg.substring(0, index);
-                    var privatemsg = msg.substring(index + 1 );
-                    if (name in users){
+                    var privatemsg = msg.substring(index + 1);
+
+                    if(name in users){
                         users[name].emit('whisper',{
                             msg: privatemsg,
                             nick: socket.nickname
                         });
+
                         socket.emit('whisper',{
                             msg: privatemsg,
                             nick: socket.nickname
                         });
+
                     }else{
-                        cb('Error! Porfavor entra un usuario validado');
+                        cb('Error! Por favor entra un usuario validado');
                     }
+
                 }else{
-                    cb('Error! Porfavor ingresa tu mensaje')
+                    cb('Error! Por favor ingresa tu mensaje');
                 }
+
             }else{
 
                 var newMsg = new Chat({
                     nick: socket.nickname,
                     msg: msg
                 });
+
                 await newMsg.save();
+
+                const total = await Chat.countDocuments();
+
+                if(total > 8){
+                    const oldMessages = await Chat.find()
+                        .sort({ created_at: 1 })
+                        .limit(total - 8);
+
+                    for(let msg of oldMessages){
+                        await Chat.deleteOne({ _id: msg._id });
+                    }
+                }
 
                 io.sockets.emit('Nuevo mensaje', {
                     msg: newMsg.msg,
                     nick: newMsg.nick
                 });
             }
-            
         });
 
         socket.on('disconnect', () => {
-            if (!socket.nickname) return;
+            if(!socket.nickname) return;
             delete users[socket.nickname];
             updateNicknames();
         });
@@ -75,6 +94,6 @@ module.exports = function(io){
         function updateNicknames(){
             io.sockets.emit('usernames', Object.keys(users));
         }
-        
+
     });
 };
